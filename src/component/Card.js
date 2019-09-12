6922822a536ab7a4d6c5b46b25f6b3c12c9c5a2a
Rachel Givens <rachelgivens@me.com>
2019-09-11 23:09:29 -0500

    added coding for components

---
+++ src/component/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Card = props => (
+	<div
+		className="cardStyle"
+		onClick={() => props.setClicked(props.id)}>
+		<div className="imgContainer">
+			<img
+				className="img"
+				alt={props.name}
+				src={props.image}
+			/>
+		</div>
+		<div className="content">
+			{props.name}
+		</div>
+	</div>
+);
+
+export default Card;
---
+++ src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = props => (
+
+    <div className="footer-copyright py-3 text-center text-white">
+        &copy; 2019 Rachel Givens
+    </div>
+);
+
+export default Footer;
