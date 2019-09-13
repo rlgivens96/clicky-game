e9725bfd10a74fbaf276a2bdf1e7c9d7f38c7ddd
Rachel Givens <rachelgivens@me.com>
2019-09-12 23:12:05 -0500

    added css to footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "./node_modules/react";
 
 const Footer = props => (
 
-    <div className="footer-copyright py-3 text-center text-white">
+    <div className="footer-copyright py-3 text-center">
         &copy; 2019 Rachel Givens
     </div>
 );
